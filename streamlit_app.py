9336acb4d1a2000dee9fe0c614e11253acd805c3
Bhanu Prakash <131232954+prakash2670@users.noreply.github.com>
2024-11-19 17:54:01 +0000

    first

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Healthy Food / Diet Recommendation System")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
